332e2884857f3925cf392735fb7e5bdaf349b0fb
victormora1 <victorjmora1@gmail.com>
2023-07-17 21:54:07 -0400

    moved all of the files in the develop folder into the main folder

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../Develop/config/connection');
 
 class ProductTag extends Model {}
 
